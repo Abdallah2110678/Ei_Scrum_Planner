0a39a1f258fa483d18d490c26bf6e79d604803f2
Ahmed22Ayman <ahmed2107685@miuegypt.edu.eg>
2024-11-14 16:17:25 +0200

    django setting.py

--- EI_SCRUM_PLANNER/EI_SCRUM_PLANNER/settings.py
+++ EI_SCRUM_PLANNER/EI_SCRUM_PLANNER/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Frontend.apps.Frontendconfig'
+    'Frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
