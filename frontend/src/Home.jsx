cca40c4451c2b0adb0dc0fae7f1f3e009650d308
Abdallah Hamdy <96912938+Abdallah2110678@users.noreply.github.com>
2025-01-31 23:56:16 +0200

    test commit

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       {/* Navbar */}
       <Navbar />
 
-      {/* Main Content */}
+      {/* Main Content there is nothing here */}
       <div className="home-container">
         {/* Sidebar */}
         <Sidebar />
