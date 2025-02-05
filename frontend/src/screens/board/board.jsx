7f25709b339e64082a81227bd85e0b5fb9f1b209
aMoni3m <abdulrahman2111656@miuegypt.edu.eg>
2025-02-05 21:08:40 +0200

    edit board page

--- frontend/src/screens/board/board.jsx
+++ frontend/src/screens/board/board.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import './board.css';
 import { Link } from 'react-router-dom';
+import TaskCard from '../../components/taskCard/taskCard';
+import { useEffect } from 'react';
+import { useDispatch ,useSelector } from 'react-redux';
+import { fetchSprints } from '../../features/sprints/sprintSlice';
+
 
 const Board = ({ toggleComponent }) => {
+  const dispatch = useDispatch();
+  const { sprints } = useSelector((state) => state.sprints);
+
+  useEffect(() => {
+    dispatch(fetchSprints());
+  }, [dispatch]);
+
+  // Filter active sprints
+  const activeSprints = sprints.filter((sprint) => sprint.is_active);
+
   return (
     <div className="board-container">
       {/* Projects / School as hyperlinks */}
@@ -13,14 +28,14 @@ const Board = ({ toggleComponent }) => {
       </div>
       
       <h2>Board</h2>
-      
+
       {/* Search Section */}
       <div className="search-section">
         <div className="search-bar">
           <input type="text" placeholder="Search" className="search-input" />
         </div>
       </div>
-      
+
       {/* Columns Section */}
       <div className="columns-section">
         {/* TO DO Column */}
@@ -29,45 +44,59 @@ const Board = ({ toggleComponent }) => {
             <h3>TO DO</h3>
           </div>
           <div className="column-content">
-            <div className="empty-state">
-              <img 
-                src="https://jira-frontend-bifrost.prod-east.frontend.public.atl-paas.net/assets/agile.52407441.svg" 
-                alt="Agile Icon" 
-              />
-              <p>Get started in the backlog</p>
-              <p>Plan and start a sprint to see issues here.</p>
-              {/* Updated Link component to use onClick handler */}
-              <Link 
-                to="/backlog" 
-                className="go-to-backlog"
-                onClick={(e) => {
-                  e.preventDefault(); // Prevent default navigation
-                  toggleComponent('backlog');
-                }}
-              >
-                Go to Backlog
-              </Link>
-            </div>
+            {activeSprints.flatMap((sprint) =>
+              sprint.tasks
+                .filter((task) => task.status === "TO DO")
+                .map((task) => (
+                  <TaskCard
+                    key={task.id}
+                    description={task.task_name}
+            
+                    count={task.story_points}
+                  />
+                ))
+            )}
           </div>
         </div>
-        
+
         {/* IN PROGRESS Column */}
         <div className="column">
           <div className="column-header">
             <h3>IN PROGRESS</h3>
           </div>
           <div className="column-content">
-            {/* Add issues or tasks here */}
+            {activeSprints.flatMap((sprint) =>
+              sprint.tasks
+                .filter((task) => task.status === "IN PROGRESS")
+                .map((task) => (
+                  <TaskCard
+                    key={task.id}
+                    description={task.task_name}
+            
+                    count={task.story_points}
+                  />
+                ))
+            )}
           </div>
         </div>
-        
+
         {/* DONE Column */}
         <div className="column">
           <div className="column-header">
             <h3>DONE</h3>
           </div>
           <div className="column-content">
-            {/* Add issues or tasks here */}
+            {activeSprints.flatMap((sprint) =>
+              sprint.tasks
+                .filter((task) => task.status === "DONE")
+                .map((task) => (
+                  <TaskCard
+                    key={task.id}
+                    description={task.task_name}
+                    count={task.story_points}
+                  />
+                ))
+            )}
           </div>
         </div>
       </div>
