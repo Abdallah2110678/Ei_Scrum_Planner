9e25610e9cf8fa876f368e9a064b887ef202c0c5
aMoni3m <abdulrahman2111656@miuegypt.edu.eg>
2025-04-19 21:11:50 +0200

    fix error in urls

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from .views import TaskViewSet,train_effort_model, predict_effort_view, task_meta_view,rework_effort_view
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename='task')
 
+
+# The URL patterns for the task app
 urlpatterns = [
+    path('', include(router.urls)),
     path("train/", train_effort_model),
     path('tasks/meta/', task_meta_view),
     path('rework-effort/', rework_effort_view),
